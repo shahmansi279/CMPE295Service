a554c68153ca8e4e410bc38d100ab7bd526d185b
shahmansi279 <shah.mansi279@gmail.com>
2016-03-08 21:14:18 -0800

    Committed Changes for Settings

--- Service/smartretail/smartretail/settings.py
+++ Service/smartretail/smartretail/settings.py
@@ -111,6 +111,39 @@ MEDIA_URL='/media/'
 
 MEDIA_ROOT=(os.path.join(BASE_DIR, "static", "media"))
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'smartretailapp': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
 
 
 
