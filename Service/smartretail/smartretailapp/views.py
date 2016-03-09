4a036f6e32d7bad14ea5ab54f3273c07a09314cb
jessiedeot <jessiedeot@gmail.com>
2016-03-09 15:35:24 -0800

    Added GET, POST and DELETE functions for CartList and CartDetail.

--- Service/smartretail/smartretailapp/views.py
+++ Service/smartretail/smartretailapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from models import NProduct,NProductClass, NAisle, NListInfo, NListPrd, NCartInfo, NCartPrd, NOffers, NCustomer, NSensors, NStore, NSalesFact1997, NProdStore, NTimeByDay,NAllDeptPdt,NAvailProducts
 from serializers import ProductSerializer, CategorySerializer, ListSerializer, CartSerializer, AisleSerializer, OfferSerializer, UserSerializer, CustomerSerializer,SensorSerializer,StoreSerializer,SalesFactSerializer,ProductStoreSerializer,AvailDeptSerializer,AvailProductsSerializer
 from django.contrib.auth import authenticate, login, logout
@@ -189,17 +192,64 @@ class AisleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset=NAisle.objects.all()
     serializer_class=AisleSerializer
 
-
+'''
 class CartList(generics.ListCreateAPIView):
 
     queryset=NCartInfo.objects.all()
     serializer_class=CartSerializer
 
-
 class CartDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset=NCartInfo.objects.all()
     serializer_class=CartSerializer
+
+'''
+class CartList(APIView):
+
+    def get(self, request, format=None):
+        carts = NCartInfo.objects.all()
+        serializer = CartSerializer(carts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CartSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CartDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return NCartInfo.objects.get(pk=pk)
+        except NCartInfo.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        cart = self.get_object(pk)
+        cart = CartSerializer(cart)
+        return Response(cart.data)
+
+    def put(self, request, pk, format=None):
+        cart = self.get_object(pk)
+        serializer = CartSerializer(cart, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        cart = self.get_object(pk)
+        cart.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
 
 class ListList(generics.ListCreateAPIView):
